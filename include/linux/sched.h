36fd01debcb3aca7b196e136eb89412672051322
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-06-28 20:58:59 +0800

    Revert "kernel: support Droidspace"
    This reverts commit 2aa77ff71f1323b6298ea00fa02742f4cd191bc5.
    
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1077,8 +1077,8 @@ struct task_struct {
 	struct nameidata		*nameidata;
 
 #ifdef CONFIG_SYSVIPC
-	// struct sysv_sem			sysvsem;
-	// struct sysv_shm			sysvshm;
+	struct sysv_sem			sysvsem;
+	struct sysv_shm			sysvshm;
 #endif
 #ifdef CONFIG_DETECT_HUNG_TASK
 	unsigned long			last_switch_count;
@@ -1511,14 +1511,9 @@ struct task_struct {
 	struct callback_head		l1d_flush_kill;
 #endif
 
-#ifdef CONFIG_SYSVIPC
-	ANDROID_KABI_USE(1, struct sysv_sem sysvsem);
-	_ANDROID_KABI_REPLACE(ANDROID_KABI_RESERVE(2); ANDROID_KABI_RESERVE(3), struct sysv_shm sysvshm);
-#else
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
-#endif
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
 	ANDROID_KABI_RESERVE(6);
